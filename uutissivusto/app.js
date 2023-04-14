b1533a594a54fa844f560f6374d397732c0ec9e5
Elias Ojala <elkkuve@gmail.com>
2023-04-14 11:58:32 +0300

    update

---
+++ uutissivusto/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+const { port, host } = require('./config.json');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'uutissivusto'
+});
+
+//serve static files
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(port, host, () => {
+    console.log(`Server running on http://${host}:${port}`);
+});
+
+
