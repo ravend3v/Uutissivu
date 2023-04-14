88627a06e608ee55407485109fb4bb41ba1f01ed
Elias Ojala <elkkuve@gmail.com>
2023-04-14 14:26:36 +0300

    update

--- uutissivusto/app.js
+++ uutissivusto/app.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const jsdom = require("jsdom");
 
 const app = express();
 const { port, host } = require('./config.json');
 
 
-const connection = mysql.createConnection({
+const connection1 = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'uutissivusto'
+});
+
+const connection2 = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'saatiedot'
+});
+
+const connection3 = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password: '',
+    database: 'blogit'
+});
+
+//Get all news
+connection1.query('SELECT * FROM uutiset', (err, results) => {
+    if (err) throw err;
+});
+
+//get the weather 
+app.get('/weather', (req, res) => {
+    connection2.query('SELECT * FROM saa WHERE vko = 21 ', (err, results) => {
+      console.log(results);
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error');
+        } else {
+          res.json(results);
+        }
+      });
 });
 
 //serve static files
---
+++ uutissivusto/public/index.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function() {
+  const weatherDataDiv = document.getElementById('weather-data');
+
+  function updateWeatherData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/weather');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const weatherData = JSON.parse(xhr.responseText);
+            let weatherHtml = '';
+            weatherData.forEach(function(item) {
+                const date = new Date(item.pvm);
+                const formattedDate = date.toLocaleDateString('fi-FI', { timeZone: 'UTC' });
+                weatherHtml += `
+                  <h1>Viikon sää -viikko${item.vko}</h1>
+                  <div class="day-box">
+                    <div class="date">${formattedDate} </div>
+                    <div class="temp">${item.lampotila}°C </div>
+                    <h2>Säätila:</h2>
+                    <div class="conditions">${item.saatila} </div>
+                    Tuulen nopeus:
+                    <div class="wind">${item.tuulennopeus} m/s </div>
+                  </div>
+                `;
+            });
+            weatherDataDiv.innerHTML = weatherHtml;
+        }
+    }
+    xhr.send();
+  }
+
+updateWeatherData();
+setInterval(updateWeatherData, 60000);
+});  
