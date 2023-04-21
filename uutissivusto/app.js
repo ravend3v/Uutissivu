d65bf7558dd05047d9d873a7411249fc7509aa9b
Elias Ojala <elkkuve@gmail.com>
2023-04-21 12:31:14 +0300

    update

--- uutissivusto/app.js
+++ uutissivusto/app.js
@@ -19,26 +19,87 @@ const yhteys = mysql.createConnection({
     database: dbconfig.database
 });
 
+app.set('view engine', 'ejs');
 
-//Haetaan kaikki uutiset tietokannasta
-app.get('/news', (req, res) => {
-    yhteys.query('SELECT * FROM uutiset ORDER BY julkaisuaika DESC', (err, vastaus) => {
-        if (err) {
-          console.error(err);
-          res.status(500);
-        } else {
-          res.json(vastaus);
-        }
-      });
+app.set('views', path.join(__dirname, 'sivupohjat'));
+
+app.get('/', (req, res) => {
+    res.render('main')
+});
+
+app.get('/saa/:id', async (req, res) => {
+  try {
+    const response = await fetch(`http://localhost:8081/saa/${req.params.id}`, {
+      headers: {
+        'Content-Type': 'application/xml'
+      },
+      mode: 'cors'
+    });
+
+    const data = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, 'text/xml');
+
+    const vko = xmlDoc.getElementsByTagName('vko')[0].childNodes[0].nodeValue;
+    const pvm = new Date(xmlDoc.getElementsByTagName('pvm')[0].childNodes[0].nodeValue).toLocaleDateString('fi-FI');
+    const lampotila = xmlDoc.getElementsByTagName('lampotila')[0].childNodes[0].nodeValue;
+    const tuuli = xmlDoc.getElementsByTagName('tuulennopeus')[0].childNodes[0].nodeValue;
+    const saatila = xmlDoc.getElementsByTagName('saatila')[0].childNodes[0].nodeValue;
+
+    const saaData = {
+      pvm: pvm,
+      lampotila: lampotila,
+      saatila: saatila,
+      tuuli: tuuli
+    };
+
+    const vkoData = {
+      vko: vko 
+    };
+
+    var results = [];
+
+    //add the data to the results array
+    results.push(saaData);
+    results.push(vkoData);
+
+    res.render('saa', { 
+      data: results
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+});
+
+app.get('/blogit', async (req, res) => {
+  try {
+    const response = await fetch('http://localhost:8080/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors'
+    });
+
+    const data = await response.json();
+    const tarinatData = data.map(d => {
+      return {
+        blogi:d.blogi,
+        otsikko:d.otsikko,
+        kirjoittaja:d.kirjoittaja
+      };
+    });
+      
+    res.render('blogit', { tarinatData });
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
 //määritellään staattisten tiedostojen hakemisto
 app.use(express.static(path.join(__dirname, 'public')));
 
-//lähetetään index.html tiedosto kun käyttäjä menee osoitteeseen http://localhost:8082/
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
 
 //Käynnistetään palvelin
 app.listen(port, host, () => {
